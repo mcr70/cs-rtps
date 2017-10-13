f573f6edc5f6405fd7674be3179b3e3d122b06e0
Mika Riekkinen <mika.riekkinen@gmail.com>
2017-10-13 08:10:26 +0300

    Initial implementatoin of RtpsMessageReceiver

---
+++ rtps/RTPSMessageReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using rtps.message;
+
+namespace rtps {
+    public class RTPSMessageReceiver {
+        private static readonly log4net.ILog Log = 
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        private readonly BlockingCollection<byte[]> _queue;
+        private bool _running = true;
+        
+        public RTPSMessageReceiver(BlockingCollection<byte[]> queue) {
+            _queue = queue;
+        }
+
+        public void Run() {
+            while (_running) {
+                try {
+                    byte[] bytes = _queue.Take();
+                    Message m = new Message(new RtpsByteBuffer(bytes));
+                    handleMessage(m);
+                }
+                catch (Exception e) {
+                    Log.Warn("Exception occured during message handling", e);                    
+                }
+            }
+        }
+
+        private void handleMessage(Message message) {
+        }
+    }
+}
